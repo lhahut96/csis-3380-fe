94033a668b687225ee0bbeb38adce3e811b05cb2
Daniel <dyepes.dev@gmail.com>
2024-08-02 12:48:25 -0700

    Creating the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { DatePicker } from "antd";
-import Creating from "./pages/creating.jsx"
+// import Creating from "./pages/creating.jsx"
+import Header from "./pages/header.jsx"
 // import "./App.css";
 
 function App() {
   return (
     <>
       {/* <DatePicker /> */}
-      <Creating/>
+      <Header/>
     </>
   );
 }
